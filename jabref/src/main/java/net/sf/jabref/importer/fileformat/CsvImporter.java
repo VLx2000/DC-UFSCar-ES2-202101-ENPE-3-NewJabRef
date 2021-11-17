9c82b6b36b05e396368c69a65764c1992430443b
VLx2000 <victor.antunes.2000@gmail.com>
2021-11-16 23:26:14 -0300

    implementação import csv

--- jabref/src/main/java/net/sf/jabref/importer/fileformat/CsvImporter.java
+++ jabref/src/main/java/net/sf/jabref/importer/fileformat/CsvImporter.java
@@ -22,6 +22,12 @@ public class CsvImporter extends ImportFormat {
     }
 
     @Override
+    public boolean isRecognizedFormat(InputStream in) throws IOException {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
     public List<BibEntry> importEntries(InputStream stream, OutputPrinter status) throws IOException {
         if (stream == null) {
             throw new IOException("No stream given.");
@@ -34,63 +40,40 @@ public class CsvImporter extends ImportFormat {
             // Preprocess entries
             String str;
 
+            List<BibEntry> results = new LinkedList<>();
+
             while ((str = in.readLine()) != null) {
-                System.out.println("oba");
-                System.out.println(str);
-                if (str.length() < 4) {
-                    continue;
+                String[] word = str.split(",");
+                BibEntry b;
+                System.out.println(word[0]);
+                if (word[0].contains("book")) {
+                    b = new BibEntry(DEFAULT_BIBTEXENTRY_ID, "book");
+                } else {
+                    b = new BibEntry(DEFAULT_BIBTEXENTRY_ID, "article");
                 }
-                /*
-                String code = str.substring(0, 4);
 
-                if ("\n".equals(code)) {
-                    sb.append('\n').append(str);
-                } else {*/
-                sb.append(str).append('\n');
-                //}
+                System.out.println(word[1]);
+                System.out.println(word[2]);
+                System.out.println(word[3]);
+                System.out.println(word[4]);
+                System.out.println(word[5]);
+                System.out.println(word[6]);
+                System.out.println(word[7]);
+                System.out.println(word[8]);
+                System.out.println(word[9]);
+
+                setOrAppend(b, "title", word[1], " ");
+                setOrAppend(b, "author", word[2], " ");
+                setOrAppend(b, "year", word[3], " ");
+                setOrAppend(b, "publisher", word[4], " ");
+                setOrAppend(b, "series", word[5], ", ");
+                setOrAppend(b, "isbn", word[6], ", ");
+                setOrAppend(b, "keywords", word[7], ", ");
+                setOrAppend(b, "note", word[8], ", ");
+                results.add(b);
             }
+            return results;
         }
-
-        if (sb.length() > 0) {
-            entries.add(sb.toString());
-        }
-        System.out.println(entries);
-
-        List<BibEntry> results = new LinkedList<>();
-
-        for (String entry : entries) {
-
-            BibEntry b = new BibEntry(DEFAULT_BIBTEXENTRY_ID, "book");
-
-            String[] word = entry.split(",");
-
-            //for (String line1 : lines) {
-
-            /*String line = line1.trim();
-            String[] word = line.split(",");*/
-            System.out.println(word[0]);
-            System.out.println(word[1]);
-            System.out.println(word[2]);
-            System.out.println(word[3]);
-            System.out.println(word[4]);
-            System.out.println(word[5]);
-            /*
-                setOrAppend(b, "title", word[0], " ");
-                setOrAppend(b, "author", word[1], " ");
-                setOrAppend(b, "year", word[2], " ");
-                setOrAppend(b, "publisher", word[3], " ");
-                setOrAppend(b, "series", word[4], ", ");
-                setOrAppend(b, "isbn", word[5], ", ");
-                setOrAppend(b, "keywords", word[6], ", ");
-                setOrAppend(b, "note", word[7], ", ");
-                setOrAppend(b, "physicaldimensions", word[8].trim(), ", ");
-                setOrAppend(b, "documenttype", word[9].trim(), ", ");*/
-
-                //}
-            results.add(b);
-        }
-
-        return results;
     }
 
     private static void setOrAppend(BibEntry b, String field, String value, String separator) {
@@ -99,11 +82,5 @@ public class CsvImporter extends ImportFormat {
         } else {
             b.setField(field, value);
         }
-    }
-
-    @Override
-    public boolean isRecognizedFormat(InputStream in) throws IOException {
-        // TODO Auto-generated method stub
-        return true;
     }
 }
